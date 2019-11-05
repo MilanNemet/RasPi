984315e5febabb3e69e6c7544c95480ca9b4809b
Németh Milán <mail.mnemeth@gmail.com>
2019-11-05 20:28:14 +0100

    new button arrows,style,logic

--- Client side/ClientSideLogic.js
+++ Client side/ClientSideLogic.js
@@ -13,13 +13,11 @@ window.onload = function () {
 
 function press(e) {
     e.target.style.background = "skyblue";
-    e.target.style.fontWeight = "bold";
     //start the corresponding engine(s)
 }
 
 function release(e) {
     e.target.style.background = "lightgrey";
-    e.target.style.fontWeight = "normal";
     //stop the corresponding engine(s)
 }
 
@@ -29,19 +27,15 @@ function keyOn(e) {
     switch (key) {
         case 37: //left
             document.getElementById("left").style.background = "skyblue";
-            document.getElementById("left").style.fontWeight = "bold";
             break;
         case 38: //up
             document.getElementById("up").style.background = "skyblue";
-            document.getElementById("up").style.fontWeight = "bold";
             break;
         case 39: //right
             document.getElementById("right").style.background = "skyblue";
-            document.getElementById("right").style.fontWeight = "bold";
             break;
         case 40: //down
             document.getElementById("down").style.background = "skyblue";
-            document.getElementById("down").style.fontWeight = "bold";
             break;
     }
     //start the corresponding engine(s)
@@ -53,19 +47,15 @@ function keyOff(e) {
     switch (key) {
         case 37: //left
             document.getElementById("left").style.background = "lightgrey";
-            document.getElementById("left").style.fontWeight = "normal";
             break;
         case 38: //up
             document.getElementById("up").style.background = "lightgrey";
-            document.getElementById("up").style.fontWeight = "normal";
             break;
         case 39: //right
             document.getElementById("right").style.background = "lightgrey";
-            document.getElementById("right").style.fontWeight = "normal";
             break;
         case 40: //down
             document.getElementById("down").style.background = "lightgrey";
-            document.getElementById("down").style.fontWeight = "normal";
             break;
     }
     //stop the corresponding engine(s)
