7c45195e2f4e1133f9226599628c6239d04e7fdf
milan.nemeth@sulid.hu <milan.nemeth@sulid.hu>
2019-12-04 15:49:48 +0100

    css optimization + chart.js included

--- Client side/ClientSideLogic.js
+++ Client side/ClientSideLogic.js
@@ -1,7 +1,49 @@
-window.onload = function () {
+window.onload = function () {/************************** MAIN FUNCTION ENTRY POINT **************************/
+
     initButtonsControlDefault();
     initKeysControlDefault();
     initToggleButton();
+
+
+    /********************** INCLUDE CHART.JS **********************/
+
+    var ctx = document.getElementById('x-axis-acceleration');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
 }
 
 
@@ -67,13 +109,21 @@ function msgStopId(id) {
 
 /********************** WEBSOCKET STUFF **********************/
 const mySocket = new WebSocket('wss://echo.websocket.org/');
-mySocket.addEventListener('open', function (event) { console.log("connection is living"); mySocket.send(JSON.stringify(msgGreeting)); });
+mySocket.addEventListener('open', function (event) { console.log("Connection successful: " + mySocket.url); mySocket.send(JSON.stringify(msgGreeting)); });
 mySocket.addEventListener('message', function (event) { console.log(event.data); });
 
 function sendMessage(obj) {
     mySocket.send(JSON.stringify(obj))
 }
 
+var sdXaAcc = 0;
+var sdXaVel = 0;
+
+var sdYaAcc = 0;
+var sdYaVel = 0;
+
+var sdZaAcc = 0;
+var sdZaVel = 0;
 
 /********************** INITIALIZERS FOR CONTROLS **********************/
 
@@ -264,8 +314,11 @@ function setStop(thisButton, obj) {
 
 
 /********************** KEYBOARD SCROLL DISABLED **********************/
+
 window.addEventListener("keydown", function (e) {
     if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
         e.preventDefault();
     }
 }, false);
+
+
