862d9b129f00d2e2c8127e660fcfc2bb0d8d490f
MilanNemet <mail.mnemeth@gmail.com>
2020-03-14 15:18:34 +0100

    static public StartConnecting(ws)
    +remove bullshits

--- Raspberry/WebSocketContext.cs
+++ Raspberry/WebSocketContext.cs
@@ -9,9 +9,6 @@ namespace RasPi
 {
     class WebSocketContext
     {
-        private bool _notFinished = true;
-        private byte _attempts = 0;
-
         public WebSocketContext(MotorController motorController, SensorController sensorController)
         {
             InitContext(motorController, sensorController);
@@ -19,66 +16,50 @@ namespace RasPi
 
         void InitContext(MotorController motorController, SensorController sensorController)
         {
-            while (_notFinished && _attempts < Byte.MaxValue)
+            using (var ws = new WebSocket(ConfigurationManager.AppSettings["WebSocketAddress"]))
             {
-                try
+                ws.OnOpen += (sender, e) =>
                 {
-                    using (var ws = new WebSocket(ConfigurationManager.AppSettings["WebSocketAddress"]))
-                    {
-                        ws.OnOpen += (sender, e) =>
-                        {
-                            Console.WriteLine("WS OPEN");
-                        };
-                        ws.OnError += (sender, e) =>
-                        {
-                            Console.WriteLine("WS ERROR");
-                        };
-                        ws.OnClose += (sender, e) =>
-                        {
-                            Console.WriteLine("WS CLOSED");
-                        };
-
-                        ws.OnMessage += (sender, e) =>
-                        {
-                            Console.WriteLine(e.Data);
-                        };
-
-                        ws.OnMessage += motorController.HandleCommand;
-                        ws.Connect();
-
-                        sensorController.WS = ws;
-                        var senderThread = new Thread(sensorController.StartTransmission);
-                        senderThread.IsBackground = true;
-                        senderThread.Start();
-
-                        var str = "";
-                        while (str != "exit")
-                        {
-                            str = Console.ReadLine();
-                            ws.Send(str);
-                        }
-                        Thread.Sleep(500);
-                        motorController.Dispose();
-                        sensorController.Dispose();
-                    }
-                }
-                catch
+                    Console.WriteLine("WS OPEN");
+                };
+                ws.OnError += (sender, e) =>
                 {
-                    ++_attempts;
-                    //	  Console.WriteLine(e);
-                    Console.WriteLine("WebSocket failure...");
-                    Console.WriteLine("Reconnecting in 20 seconds...");
-                    for (int i = 0; i < 20; ++i)
-                    {
-                        Console.Write(".");
-                        Thread.Sleep(1000);
-                    }
-                }
-                finally
+                    Console.WriteLine("WS ERROR");
+                };
+                ws.OnClose += (sender, e) =>
                 {
-                    motorController.Dispose();
-                    sensorController.Dispose();
+                    Console.WriteLine("WS CLOSED");
+                };
+                ws.OnMessage += (sender, e) =>
+                {
+                    Console.WriteLine(e.Data);
+                };
+                ws.OnMessage += motorController.HandleCommand;
+
+                StartConnecting(ws);
+
+                sensorController.WS = ws;
+                var senderThread = new Thread(sensorController.StartTransmission);
+                senderThread.IsBackground = true;
+                senderThread.Start();
+
+                var str = "";
+                while (str != "exit")
+                {
+                    str = Console.ReadLine();
+                    ws.Send(str);
                 }
+                Thread.Sleep(500);
+                motorController.Dispose();
+                sensorController.Dispose();
+            }
+        }
+        static void StartConnecting(WebSocket ws)
+        {
+            while (!ws.IsAlive)
+            {
+                Console.WriteLine("WS CONNECTING...");
+                ws.Connect();
             }
         }
     }
