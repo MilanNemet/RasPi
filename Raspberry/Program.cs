fd95916ad518b77f9bf271ce37eac35c055d4ff9
MilanNemet <mail.mnemeth@gmail.com>
2020-03-21 19:10:43 +0100

    clear unuseful comments

--- Raspberry/Program.cs
+++ Raspberry/Program.cs
@@ -10,22 +10,6 @@ namespace RasPi
     {
         static void Main(string[] args)
         {
-            //string js1 = "{\"Type\":\"control\",\"Value\":\"SL\",\"TimeStamp\":\"2020-02-28T17:29:34.486Z\"}";
-            //string js2 = "{\"Type\":\"control\",\"Value\":\"SF\",\"TimeStamp\":\"2020-02-28T17:42:53.512Z\"}";
-
-            //var mb1 = new MessageBlock(js1);
-            //var mb2 = new MessageBlock(js2);
-
-            //Console.WriteLine(mb1.Type);
-            //Console.WriteLine(mb1.Value);
-            //Console.WriteLine(mb1.TimeStamp);
-
-            //Console.WriteLine();
-
-            //Console.WriteLine(mb2.Type.ToUpper());
-            //Console.WriteLine(mb2.Value.ToUpper());
-            //Console.WriteLine(mb2.TimeStamp.ToString().ToUpper());
-
             new WebSocketContext(new MotorController(), new SensorController());
         }
     }
