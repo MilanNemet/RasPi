3ead35e9bacfbbdaf98949206f14e9b2f0515123
MilanNemet <mail.mnemeth@gmail.com>
2020-03-21 19:09:13 +0100

    new prop, new ctor, refactoring

--- Raspberry/MessageBlock.cs
+++ Raspberry/MessageBlock.cs
@@ -6,16 +6,26 @@ namespace RasPi
 {
     public class MessageBlock
     {
+        public string Type { get; set; }
+        public int UserId { get; private set; }
+        public string Value { get; set; }
+        public DateTime TimeStamp { get; set; }
+
         public MessageBlock() { }
+        public MessageBlock(float[] value)
+        {
+            Type = "control";
+            UserId = 0;
+            Value = JsonSerializer.Serialize(value);
+            TimeStamp = DateTime.Now;
+        }
         public MessageBlock(string jsonstring)
         {
             var mb = JsonSerializer.Deserialize<MessageBlock>(jsonstring);
             Type = mb.Type;
+            UserId = mb.UserId;
             Value = mb.Value;
             TimeStamp = mb.TimeStamp;
         }
-        public string Type { get; set; }
-        public string Value { get; set; }
-        public DateTime TimeStamp { get; set; }
     }
 }
